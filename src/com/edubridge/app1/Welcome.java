85e8a322e154785db5ad8ec865f038279a5c5965
Anilkumar1234a <151600050+Anilkumar1234a@users.noreply.github.com>
2023-11-21 11:12:53 +0530

    Update Welcome.java

--- src/com/edubridge/app1/Welcome.java
+++ src/com/edubridge/app1/Welcome.java
@@ -3,7 +3,7 @@ package com.edubridge.app1;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("welcome ANIL");
+		System.out.println("WELCOME TO MY DARKSIDE ");
 	}
 
 }
